5d46f877a52f095d99c2a8bafe287eeeba9f63cc
Samantha Orozco <orozcoxs@gmail.com>
2023-02-02 10:17:12 -0800

    index and css basics completed

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+var apiKey = "dda2361f9711b148f9a98283d4756472";
+
+fetch("api.openweathermap.org/data/2.5/weather?q=London&appid=dda2361f9711b148f9a98283d4756472")
+
+// function searchApi(query, format) {
+//     var locQueryUrl = 'https://www.loc.gov/search/?fo=json';
+
+//     if (format) {
+//         locQueryUrl = 'https://www.loc.gov/' + format + '/?fo=json';
+//     }
+
+//     locQueryUrl = locQueryUrl + '&q=' + query;
+
+//     fetch(locQueryUrl)
+//         .then(function (response) {
+//             if (!response.ok) {
+//                 throw response.json();
+//             }
+
+//             return response.json();
+//         })
+//         .then(function (locRes) {
+//             // write query to page so user knows what they are viewing
+//             resultTextEl.textContent = locRes.search.query;
+
+//             console.log(locRes);
+
+//             if (!locRes.results.length) {
+//                 console.log('No results found!');
+//                 resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
+//             } else {
+//                 resultContentEl.textContent = '';
+//                 for (var i = 0; i < locRes.results.length; i++) {
+//                     printResults(locRes.results[i]);
+//                 }
+//             }
+//         })
+//         .catch(function (error) {
+//             console.error(error);
+//         });
+// }
