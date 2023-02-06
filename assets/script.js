aec50fd194d2597c869f0f5e8319add6b02ef699
Samantha Orozco <orozcoxs@gmail.com>
2023-02-05 16:43:01 -0800

    added main function to connect API

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,36 @@
-var apiKey = "dda2361f9711b148f9a98283d4756472";
+var key = "dda2361f9711b148f9a98283d4756472";
+var currentCity = "";
+var lon = "";
+var lat = ""; 
+var button = document.getElementById("button");
+var search = document.getElementById("search");
+var city = document.getElementById("city");
+var geoCode = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
+var fiveDay = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 
-fetch("http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={dda2361f9711b148f9a98283d4756472}")
+function displayInfo() {
+    var geoCode = "http://api.openweathermap.org/geo/1.0/direct?q="+ search.value +"&limit=1&appid="+ key +"";
+    fetch(geoCode)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            var lon = data[0].lon;
+            var lat = data[0].lat;
+            var fiveDay = "http://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ lon +"&appid="+ key +"";
+            fetch(fiveDay)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            city.textContent = data.city.name;
+        })
+        })
+}
 
-// function searchApi(query, format) {
-//     var locQueryUrl = 'https://www.loc.gov/search/?fo=json';
+button.addEventListener("click", displayInfo)
 
-//     if (format) {
-//         locQueryUrl = 'https://www.loc.gov/' + format + '/?fo=json';
-//     }'-[[[;]]]
 
-//     locQueryUrl = locQueryUrl + '&q=' + query;
 
-//     fetch(locQueryUrl)
-//         .then(function (response) {
-//             if (!response.ok) {
-//                 throw response.json();
-//             }
-
-//             return response.json();
-//         })
-//         .then(function (locRes) {
-//             // write query to page so user knows what they are viewing
-//             resultTextEl.textContent = locRes.search.query;
-
-//             console.log(locRes);
-
-//             if (!locRes.results.length) {
-//                 console.log('No results found!');
-//                 resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
-//             } else {
-//                 resultContentEl.textContent = '';
-//                 for (var i = 0; i < locRes.results.length; i++) {
-//                     printResults(locRes.results[i]);
-//                 }
-//             }
-//         })
-//         .catch(function (error) {
-//             console.error(error);
-//         });
-// }
