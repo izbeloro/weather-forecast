117962fb39a90848ae6c95e388d9508d48857fde
Samantha Orozco <orozcoxs@gmail.com>
2023-02-07 21:34:09 -0800

    fixed http bug

--- assets/script.js
+++ assets/script.js
@@ -4,11 +4,11 @@ var lat = "";
 var button = document.getElementById("button");
 var search = document.getElementById("search");
 var city = document.getElementById("city");
-var geoCode = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
+var geoCode = "https://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
 var fiveDay = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
 
 function displayInfo() {
-    var geoCode = "http://api.openweathermap.org/geo/1.0/direct?q="+ search.value +"&limit=1&appid="+ key +"";
+    var geoCode = "https://api.openweathermap.org/geo/1.0/direct?q="+ search.value +"&limit=1&appid="+ key +"";
     fetch(geoCode)
         .then(function (response) {
             return response.json();
@@ -17,7 +17,7 @@ function displayInfo() {
             console.log(data)
             var lon = data[0].lon;
             var lat = data[0].lat;
-            var fiveDay = "http://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ lon +"&appid="+ key +"&units=imperial";
+            var fiveDay = "https://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ lon +"&appid="+ key +"&units=imperial";
             fetch(fiveDay)
         .then(function (response) {
             return response.json();
