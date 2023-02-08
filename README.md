9d2f8da5cc5b51351c56e0edf0a10c2f6ce808b6
Samantha Orozco <orozcoxs@gmail.com>
2023-02-07 19:21:59 -0800

    readme added

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # weather-forecast
+
+## Description
+
+This webpage was created to display the current forecast, as well as the 5 day forecast. The user searches for the corresponding city they are looking for the weather in. Once the button "search" is clciked, it reveals the forecast for the next 5 days. This webpage displayed the acquired skills learned throughout the course displaying knowledge on HTML, CSS, CSS framework, and JavaScript. 
+
+## Tasks
+- Display data pulled from a server side API correctly.
+- Use client side storage to store persistant data.
+- Use CSS framework other than Bootstrap
+
+
+
+## Usage
+
+![ScreenShot](./assets/weather.png)
+
+https://izbeloro.github.io/weather-forecast/
+
+To use this challenge weboage, search for the desired city. Then, the current weather and the forecast for the following 5 days populates on  the  screen. The forecast also shows an icon that reflects how the weather looks currently. For example, if its cloudy, the icon shows the clouds. The recent searches the user has searched is also saved in local storage.
+
+
+## Credits
+
+- https://www.w3schools.com/jsref/prop_win_localstorage.asp
+- https://www.makeareadme.com/#license-1
+- https://openweathermap.org/api
+
+## License
+
+MIT License
+
+Copyright (c) [2023] [SamanthaOrozco] [ArianaCoia] [MelanieIslas]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
 var key = "dda2361f9711b148f9a98283d4756472";
-var currentCity = "";
 var lon = "";
 var lat = ""; 
 var button = document.getElementById("button");
@@ -18,7 +17,7 @@ function displayInfo() {
             console.log(data)
             var lon = data[0].lon;
             var lat = data[0].lat;
-            var fiveDay = "http://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ lon +"&appid="+ key +"";
+            var fiveDay = "http://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ lon +"&appid="+ key +"&units=imperial";
             fetch(fiveDay)
         .then(function (response) {
             return response.json();
@@ -26,9 +25,66 @@ function displayInfo() {
         .then(function (data) {
             console.log(data)
             city.textContent = data.city.name;
+            for (var i = 0; i < 6; i++) {
+                document.getElementById("temp-"+i+"").textContent = "Temp: " +Number(data.list[i].main.temp).toFixed(0)+ "°";
+            }
+            for (var i = 0; i < 6; i++) {
+                document.getElementById("wind-"+i+"").textContent = "Wind: " +Number(data.list[i].wind.speed).toFixed(0)+ "mph";
+            }
+            for (var i = 0; i < 6; i++) {
+                document.getElementById("humidity-"+i+"").textContent = "Humidity: " +Number(data.list[i].main.humidity).toFixed(0)+ "%";
+            }
+            for (var i = 0; i < 6; i++) {
+                document.getElementById("icon-"+i+"").src = "http://openweathermap.org/img/wn/"+ data.list[i].weather[0].icon+".png";
+            }
         })
         })
 }
+
+var weekDay = [
+    moment().format("dddd"),
+    moment().add(1,"d").format("dddd"),
+    moment().add(2,"d").format("dddd"),
+    moment().add(3,"d").format("dddd"),
+    moment().add(4,"d").format("dddd"),
+    moment().add(5,"d").format("dddd"),
+    moment().add(6,"d").format("dddd"),
+]
+for (i = 0; i < 6; i++) {
+    document.getElementById("day-"+i+"").textContent = weekDay[i];
+}
+
+window.addEventListener('load', () => {
+    var lat;
+    var lon;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            lon = position.coords.longitude;
+            lat = position.coords.latitude;
+            fiveDay = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat +'&lon=' + lon +'&appid=' + key +'&units=imperial';
+            
+            fetch(fiveDay)
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (data) {
+                    city.textContent = data.city.name;
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById("temp-"+i+"").textContent = "Temp: " +Number(data.list[i].main.temp).toFixed(0)+ "°";
+                    }
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById("wind-"+i+"").textContent = "Wind: " +Number(data.list[i].wind.speed).toFixed(0)+ "mph";
+                    }
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById("humidity-"+i+"").textContent = "Humidity: " +Number(data.list[i].main.humidity).toFixed(0)+ "%";
+                    }
+                    for (var i = 0; i < 6; i++) {
+                        document.getElementById("icon-"+i+"").src = "http://openweathermap.org/img/wn/"+ data.list[i].weather[0].icon+".png";
+                    }
+                })
+        })
+    }
+})
 
 button.addEventListener("click", displayInfo)
 
